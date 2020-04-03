44f2f3748ce495fcd423fd9056c6b942c95ec946
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-03 20:37:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-cheese_types = ["cheddar", "gouda", "camembert"]
-if array.include?(cheese_types)
-  return cheese_types[0]
-end
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each do |cheese|
+    
+  end
 end
