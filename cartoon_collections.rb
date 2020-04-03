862453ba58b78c4612c600b0097249e2255ff169
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-03 11:42:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def summon_captain_planet(planeteer_calls)# code an argument here
 end
 
 def long_planeteer_calls(array)
-  array.any? {|calls| calls > 4}
+  array.any? {|calls| calls.length > 4}
 end
 
 def find_the_cheese# code an argument here
