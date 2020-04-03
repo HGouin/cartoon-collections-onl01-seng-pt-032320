5728af699800aa44ca11f491ec65393ede431457
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-03 11:29:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,6 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.collect {|items| items.capitalize}
-
-
 end
 
 def long_planeteer_calls# code an argument here
