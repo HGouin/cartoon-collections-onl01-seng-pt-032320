41c67d7ad933e7c8353f42112db33c42d5a27c29
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-03 11:10:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
-def roll_call_dwarves(array, index)
-array.each_with_index { |name, index|}
-  puts "#{index+1}. #{name}"
+def roll_call_dwarves(dwarves)
+  i = 0
+  while i < dwarves.length
+    puts "#{dwarves}"
 end
 
 def summon_captain_planet# code an argument here
