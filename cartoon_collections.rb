527173727613d3d4512d493c5804523582cf44d3
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-03 11:16:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
 end
 
